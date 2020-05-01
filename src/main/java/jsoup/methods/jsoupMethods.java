3340402a71d349696cc4feacfcb477dd0a5807b6
be-persistent <54042004+be-persistent@users.noreply.github.com>
2020-05-01 14:09:44 -0400

    Lots of logic changes

--- src/main/java/jsoup/methods/jsoupMethods.java
+++ src/main/java/jsoup/methods/jsoupMethods.java
@@ -1,5 +1,7 @@
 package jsoup.methods;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,78 +19,100 @@ import java.util.Map.Entry;
 
 public class jsoupMethods {
 	
-	private final String boundaryDomain = "cengage";
-	private final static String urlToStart = "https://www.cengage.com/";
-	private static List<String> links = new ArrayList<String>();
+	//private static Logger logger = LogManager.getLogger(jsoupMethods.class);
+	
+	//what string to look for in a url to keep searching links, also a safeguard to prevent runaway
+	private static final String boundaryDomain = "cengage";
+	
+	//initialUrl must be the exact scheme and different domain elements TODO: make final
+	private static String initialUrl = "https://www.google.com//";
+	
+	private static HashMap<String, HashMap<String, String>> mapOfUrlsAndInvalidLinks = new HashMap<String, HashMap<String, String>>();
+	
+	private static final String[] typesOfLinks = {"href", "src"};
+	
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
+		manageLinks(initialUrl);
 		
-		utilities Utilities = new utilities();
-
-		try {
-			findLinks();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if(mapOfUrlsAndInvalidLinks.size()>0) {
+			Iterator<Entry<String, HashMap<String, String>>> iter = mapOfUrlsAndInvalidLinks.entrySet().iterator();
+			while(iter.hasNext()) {
+				Entry<String, HashMap<String, String>> urlPlusMap = iter.next();
+				String primaryUrl = urlPlusMap.getKey();
+				Iterator<Entry<String, String>> invalidIter = urlPlusMap.getValue().entrySet().iterator();
+				while(invalidIter.hasNext()) {
+					String strUrlAndCode = invalidIter.next().toString();
+					System.out.println("Failure on page [".concat(primaryUrl).concat("] ---> ").concat(strUrlAndCode));					
+				}
+			}
+			System.out.println("End of issues");
+		}else {
+			System.out.println("No issues found");
 		}
-		Map<String, String> invalidLinks = validateLinks();
-		Iterator<Entry<String, String>> iter = invalidLinks.entrySet().iterator();
 		
-		while(iter.hasNext()) {
-			System.out.println("Failure -> ".concat(iter.next().toString().replace("="," ---> ")));
-		}
-		
-		System.out.println("Done");
 	}
 	
-    private static void findLinks() throws IOException {
-        Document doc = Jsoup.connect(urlToStart).get();
+	private static void manageLinks(String url) throws IOException {
+		
+		while(url.endsWith("/")) {
+			url = url.substring(0, url.length()-1);
+		}
+		
+		ArrayList<String> links = findLinks(url);
+		
+		if(links.isEmpty()) {
+			System.out.println("No links found on page [".concat(url).concat("]"));
+		}else {
+			HashMap<String, String> invalidLinks = validateLinks(url, links);
+			if(invalidLinks.isEmpty()) {
+				System.out.println("No invalid links found on page [".concat(url).concat("]"));
+			}else {
+				mapOfUrlsAndInvalidLinks.put(url, invalidLinks);					
+			}
+		}
+	}
+	
+    private static ArrayList<String> findLinks(String url) throws IOException {
+    	ArrayList<String> links = new ArrayList<String>();
+    	
+        Document doc = Jsoup.connect(url).get();
         Elements elements = doc.select("[src]");
         elements.addAll(doc.select("[href]"));
         
         for (Element element : elements) {
-            links.add(element.attr("href"));
-            links.add(element.attr("src"));
+            links.add(element.attr("abs:href"));
+            links.add(element.attr("abs:src"));
         }
         
-        System.out.println("Total links found [" .concat(String.valueOf(links.size()).concat("]")));
+        System.out.println("Total links found (".concat(String.valueOf(links.size()).concat(")")));
+        return links;
     }
     
-    private static Map<String, String> validateLinks() {
+    private static HashMap<String, String> validateLinks(String urlOfPage, ArrayList<String> listOfLinks) {
     	URL url = null;
-    	Map<String, String> mapOfFailedLinks = new HashMap<String, String>();
+    	HashMap<String, String> mapOfFailedLinks = new HashMap<String, String>();
     	Map<String, Integer> mapOfOccurrences = new HashMap<String, Integer>();
     	int response = 0;
     	int count = 1;
     	
-    	for(String str:links) {
+    	for(String str:listOfLinks) {
     		boolean isValidUrl = true;
     		if(!str.isEmpty()) {
 				try {
 					url = new URL(str);
 				} catch (MalformedURLException e) {
-					if(urlToStart.endsWith("/") && str.startsWith("/")) {
-						if(str.startsWith("//")) {
-							str = urlToStart.concat(str.substring(2, str.length()));
-						}else {
-							str = urlToStart.concat(str.substring(1, str.length()));
-						}
+					if(mapOfOccurrences.containsKey(str)) {
+						int i = mapOfOccurrences.get(str);
+						mapOfOccurrences.put(str, i++);
+					}else {
+						mapOfOccurrences.put(str, 1);
 					}
 					
-					try {
-						url = new URL(str);
-					} catch (MalformedURLException e1) {
-						if(mapOfOccurrences.containsKey(str)) {
-							int i = mapOfOccurrences.get(str);
-							mapOfOccurrences.put(str, i++);
-						}else {
-							mapOfOccurrences.put(str, 1);
-						}
-						
-						if(!mapOfFailedLinks.containsKey(str)) {
-							mapOfFailedLinks.put(str, "MalformedURLException");
-						}
-						isValidUrl = false;
+					if(!mapOfFailedLinks.containsKey(str)) {
+						mapOfFailedLinks.put(str, "MalformedURLException");
 					}
+					isValidUrl = false;
 				}
 				
 				if(isValidUrl) {
@@ -109,14 +133,14 @@ public class jsoupMethods {
 						if(httpURLConnection != null) {
 							httpURLConnection.disconnect();
 						}
-					}					
+					}
 				}
     		}else {
     			if(!mapOfFailedLinks.containsKey("empty link")) {
     				mapOfFailedLinks.put("empty link", "no value");
     			}
     		}
-    		utilities.printPercentage(count++);
+    		utilities.printPercentage(listOfLinks.size(), count++);
     	}
     	return mapOfFailedLinks;
     }
@@ -125,16 +149,13 @@ public class jsoupMethods {
     	
     	static int previousVal;
     	
-    	private static void printPercentage(int count) {
-    		
-    		int iDiv = (count * 100 / links.size());
+    	private static void printPercentage(int size, int count) {
+    		int iDiv = (count * 100 / size);
     		
     		if(((iDiv % 10) == 0) && (iDiv != previousVal)) {
     			System.out.println("Percent complete [".concat(String.valueOf(iDiv).concat("]")));
     			previousVal = iDiv;
-    			
     		}
     	}
-    	
     }
 }
