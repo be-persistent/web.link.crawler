9be7d93d2096b8910c6adbf0ce88df5d0bd12200
be-persistent <54042004+be-persistent@users.noreply.github.com>
2020-05-02 12:39:42 -0400

    Added amout of occurances to each error change

--- src/main/java/jsoup/methods/jsoupMethods.java
+++ src/main/java/jsoup/methods/jsoupMethods.java
@@ -1,7 +1,5 @@
 package jsoup.methods;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -18,38 +16,29 @@ import java.util.Map.Entry;
 
 public class jsoupMethods {
 	
-	//private static Logger logger = LogManager.getLogger(jsoupMethods.class);
-	
-	//what string to look for in a url to keep searching links, also a safeguard to prevent runaway
-	private static final String boundaryDomain = "cengage";
-	
 	//initialUrl must be the exact scheme and different domain elements TODO: make final
 	private static String initialUrl = "https://www.cengage.com/";
 	
 	private static HashMap<String, HashMap<String, String>> mapOfUrlsAndInvalidLinks = new HashMap<String, HashMap<String, String>>();
 	
-	private static final String[] typesOfLinks = {"href", "src"};
-	
 	public static void main(String[] args) throws IOException {
 
 		manageLinks(initialUrl);
-		
+
 		if(mapOfUrlsAndInvalidLinks.size() > 0) {
 			Iterator<Entry<String, HashMap<String, String>>> iter = mapOfUrlsAndInvalidLinks.entrySet().iterator();
 			while(iter.hasNext()) {
 				Entry<String, HashMap<String, String>> urlPlusMap = iter.next();
 				String primaryUrl = urlPlusMap.getKey();
-				Iterator<Entry<String, String>> invalidIter = urlPlusMap.getValue().entrySet().iterator();
+				Iterator<String> invalidIter = urlPlusMap.getValue().keySet().iterator();
 				while(invalidIter.hasNext()) {
-					String key = invalidIter.next().getKey();
+					String key = invalidIter.next();
 					String val = urlPlusMap.getValue().get(key);
 					System.out.println("Failure on page [".concat(primaryUrl).concat("] ---> ")
 							.concat(key).concat(" [").concat(val).concat("]"));
 				}
 			}
 			System.out.println("End of issues");
-		}else {
-			System.out.println("No issues found");
 		}
 		
 	}
@@ -87,6 +76,7 @@ public class jsoupMethods {
         }
         
         System.out.println("Total links found (".concat(String.valueOf(links.size()).concat(")")));
+        
         return links;
     }
     
@@ -94,58 +84,80 @@ public class jsoupMethods {
     	URL url = null;
     	HashMap<String, String> mapOfFailedLinks = new HashMap<String, String>();
     	Map<String, Integer> mapOfOccurrences = new HashMap<String, Integer>();
+    	ArrayList<String> listOfLinksVerified = new ArrayList<String>();
     	int response = 0;
     	int count = 1;
     	
     	for(String str:listOfLinks) {
-    		boolean isValidUrl = true;
-    		if(!str.isEmpty()) {
-				try {
-					url = new URL(str);
-				} catch (MalformedURLException e) {
-					if(mapOfOccurrences.containsKey(str)) {
-						int i = mapOfOccurrences.get(str);
-						mapOfOccurrences.put(str, i++);
-					}else {
-						mapOfOccurrences.put(str, 1);
-					}
-					
-					if(!mapOfFailedLinks.containsKey(str)) {
-						mapOfFailedLinks.put(str, "MalformedURLException");
-					}
-					isValidUrl = false;
-				}
-				
-				if(isValidUrl) {
-		            HttpURLConnection.setFollowRedirects(false);
-		            HttpURLConnection httpURLConnection = null;
-					try {
-						httpURLConnection = (HttpURLConnection) url.openConnection();
-			            httpURLConnection.setRequestMethod("HEAD");
-			            response = httpURLConnection.getResponseCode();
-					} catch (IOException e) {
-			            if(!mapOfFailedLinks.containsKey(url.toString())) {
-			           		mapOfFailedLinks.put(url.toString(), e.toString());
-			            }
-					} finally {
-			            if((response >= 400 && response < 999) && (!mapOfFailedLinks.containsKey(url.toString()))) {
-			           		mapOfFailedLinks.put(url.toString(), String.valueOf(response));
-			            }
-						if(httpURLConnection != null) {
-							httpURLConnection.disconnect();
-						}
-					}
-				}
-    		}else {
-    			if(!mapOfFailedLinks.containsKey("empty link")) {
-    				mapOfFailedLinks.put("empty link", "no value");
-    			}
+    		if(!listOfLinksVerified.contains(str)) { //No need to keep verifying the same link
+    			listOfLinksVerified.add(str);
+        		boolean isValidUrl = true;
+        		if(!str.isEmpty()) {
+    				try {
+    					url = new URL(str);
+    				} catch (MalformedURLException e) {
+    					if(mapOfOccurrences.containsKey(str)) {
+    						int i = mapOfOccurrences.get(str);
+    						mapOfOccurrences.put(str, i++);
+    					}else {
+    						mapOfOccurrences.put(str, 1);
+    					}
+    					
+    					if(!mapOfFailedLinks.containsKey(str)) {
+    						mapOfFailedLinks.put(str, "MalformedURLException");
+    					}
+    					isValidUrl = false;
+    				}
+    				
+    				if(isValidUrl) {
+    		            HttpURLConnection.setFollowRedirects(false);
+    		            HttpURLConnection httpURLConnection = null;
+    					try {
+    						httpURLConnection = (HttpURLConnection) url.openConnection();
+    			            httpURLConnection.setRequestMethod("HEAD");
+    			            response = httpURLConnection.getResponseCode();
+    					} catch (IOException e) {
+    			            if(!mapOfFailedLinks.containsKey(url.toString())) {
+    			           		mapOfFailedLinks.put(url.toString(), e.toString());
+    			            }
+    			            isValidUrl = false;
+    					} finally {
+    			            if((response >= 400 && response < 999) && (!mapOfFailedLinks.containsKey(url.toString()))) {
+    			           		mapOfFailedLinks.put(url.toString(), String.valueOf(response));
+    			           		isValidUrl = false;
+    			            }
+    						if(httpURLConnection != null) {
+    							httpURLConnection.disconnect();
+    						}
+    					}
+    				}
+        		}
+        		
+        		if(!isValidUrl) {
+            		int cnt = getCountOfStringInList(listOfLinks, str);
+            		String val = mapOfFailedLinks.get(str);
+            		val = val.concat(" x").concat(String.valueOf(cnt));
+            		mapOfFailedLinks.put(str, val);
+        		}
     		}
+    		
     		utilities.printPercentage(listOfLinks.size(), count++);
     	}
     	return mapOfFailedLinks;
     }
 
+    private static int getCountOfStringInList(ArrayList<String> list, String url) {
+    	int count = 0;	
+    	if(!url.isEmpty()) {
+        	for(String str:list) {
+        		if(str.equals(url)) {
+        			count++;
+        		}
+        	}		
+    	}
+    	return count;
+    }
+    
     private static class utilities {
     	
     	static int previousVal;
